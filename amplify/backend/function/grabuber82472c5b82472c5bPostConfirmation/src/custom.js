b135b5f19f7ffafac354d301cad78c1ce4dcb345
RadoslavKolev <radoslav.kolev@abv.bg>
2022-06-28 20:07:56 +0300

    Setup Lambda trigger for Authentication

--- amplify/backend/function/grabuber82472c5b82472c5bPostConfirmation/src/custom.js
+++ amplify/backend/function/grabuber82472c5b82472c5bPostConfirmation/src/custom.js
@@ -1,7 +1,49 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+const aws = require('aws-sdk');
+const ddb = new aws.DynamoDB();
+
 exports.handler = async (event, context) => {
-  // insert code to be executed by your lambda trigger
-  return event
+  const date = new Date();
+  if (event.request.userAttributes.sub) {
+    let params = {
+      Item: {
+        'id': {
+          S: event.request.userAttributes.sub,
+        },
+        '__typename': {
+          S: 'User',
+        },
+        'username': {
+          S: event.userName,
+        },
+        'email': {
+          S: event.request.userAttributes.email,
+        },
+        'createdAt': {
+          S: date.toISOString(),
+        },
+        'updatedAt': {
+          S: date.toISOString(),
+        },
+      },
+      TableName: process.env.USERTABLE,
+    };
+
+    try {
+      await ddb.putItem(params).promise();
+      console.log('Success');
+    } catch (err) {
+      console.log('Error', err);
+    }
+
+    console.log('Success: Everything executed correctly');
+    context.done(null, event);
+
+  } else {
+    console.log('Error: Nothing was written to DynamoDB');
+    context.done(null, event);
+  }
 };
