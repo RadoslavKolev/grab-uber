96875c610e10e33e8ebc2ea1d596481c40ff211e
RadoslavKolev <radoslav.kolev@abv.bg>
2022-07-28 14:44:38 +0300

    Calculating Estimated travel time and Distance

--- src/screens/SearchResults/searchResults.js
+++ src/screens/SearchResults/searchResults.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 
-import React, { useState } from 'react';
-import { View, Dimensions, Alert } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, Dimensions, Alert } from 'react-native';
 import { API, graphqlOperation, Auth } from 'aws-amplify';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import RouteMap from '../../components/RouteMap/routeMap';
@@ -9,12 +9,37 @@ import UberTypes from '../../components/UberTypes/uberTypes';
 import { createOrder } from '../../graphql/mutations';
 
 const SearchResults = (props) => {
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyCJYRRFXU3JGkSshd9tz5ws_gdo7XvC7aM';
+
   const typeState = useState(null);
+  const ref = useRef();
 
   // Receiving data from DestinationSearch
   const route = useRoute();
   const navigation = useNavigation();
   const {originPlace, destinationPlace} = route.params;
+
+  useEffect(() => {
+    const getTravelTime = async () => {
+      await fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${originPlace.data.description}&destinations=${destinationPlace.data.description}&key=${GOOGLE_MAPS_APIKEY}`
+      )
+      .then(res => res.json())
+      .then(data => {
+        ref.current = data.rows[0].elements[0];
+      });
+    };
+
+    getTravelTime();
+  }, [originPlace, destinationPlace, GOOGLE_MAPS_APIKEY]);
+
+  const travelTime = () => {
+    return ref.current?.duration.text;
+  };
+
+  const travelDistance = () => {
+    return ref.current?.distance.text;
+  };
 
   const onSubmit = async () => {
     const [type] = typeState;
@@ -69,11 +94,37 @@ const SearchResults = (props) => {
       display: 'flex',
       justifyContent: 'space-between',
     }}>
-      <View style={{height: Dimensions.get('window'). height - 400}}>
+      <View style={{height: Dimensions.get('window'). height - 500}}>
         <RouteMap origin={originPlace} destination={destinationPlace} />
       </View>
 
-      <View style={{ height: 400, width: 406, backgroundColor: '#ffffff' }}>
+      <View ref={ref} style={{
+        height: 70,
+        width: 406,
+        backgroundColor: '#ffffff',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Text style={{
+          color: '#000',
+          fontSize: 17,
+          fontWeight: 'bold',
+          marginTop: 20,
+        }}>Distance - {travelDistance()}</Text>
+        <Text style={{
+          color: '#000',
+          fontSize: 17,
+          fontWeight: 'bold',
+          marginVertical: 15,
+        }}>Travel Time - {travelTime()}</Text>
+      </View>
+
+      <View style={{
+        height: 430,
+        width: 406,
+        backgroundColor: '#ffffff',
+      }}>
         <UberTypes
           typeState={typeState}
           onSubmit={onSubmit}
